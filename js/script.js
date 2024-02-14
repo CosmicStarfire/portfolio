db0fa9a4557342606f45e8df17d9ca1dcd617e4a
Miranda Saddleback Nanatewikamik <nanatewikamik@gmail.com>
2024-02-14 15:38:04 -0700

    added correct styles for sandwich bar

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-// FOR BUTTON DROP DOWN
-
-// Make a function that will drop down a menu.
-// Clickable, and hover. (will go away when not clicked?)
-
-// IF CLICKED, THEN MENU SHOWS. MENU STAYS WHEN IS HOVERED. WHEN YOU MOVE OFF MENU THEN IT COLLAPSE.
-
-// <div="menu-container"
-// <button="mobile-menu
-
-var mobileMenu = document.getElementById("mobile-menu");
-var navLinks = document.getElementById("nav-links-container");
-// X button
-var mobileClose = document.getElementById("mobileClose");
-// // Hamburger
-var mobileOpen = document.getElementById("mobileOpen");
-
-mobileMenu.addEventListener("click", function () {
-  // Toggle Menu Appear
-  navLinks.classList.toggle("hidden");
-  mobileClose.classList.toggle("hidden");
-  mobileOpen.classList.toggle("hidden");
-});
-
 // web dev modal
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay-la");
@@ -36,7 +12,7 @@ const openModal = function () {
   body.classList.add("fixed");
 };
 // Event listener for opening the modal when the button is clicked
-openModalBtn.addEventListener("click", openModal);
+openModalBtn.addEventListener("click", openModal());
 
 // Function for closing the modal
 const closeModal = function () {
